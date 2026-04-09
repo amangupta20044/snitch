c876649691f22ceb02b0fe1743cad67c4b80d4a3
amangupta20044 <a.mann20044@gmail.com>
2026-04-09 20:24:17 +0530

    register

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    email:{type: String, required: true, unique: true},
+    contact:{type: String, required: true},
+    password:{type: String, required: true},
+    fullname:{type: String, required: true},
+    role:{
+        type: String,
+        enum: ["buyer", "seller"],
+        default: "buyer"
+    }
+})
+// Hash the password before saving the user this is middleware that runs before saving a user to the database. It checks if the password field has been modified, and if so, it hashes the password using bcrypt with a salt round of 10. 
+// The hashed password is then stored in the database instead of the plain text password.
+userSchema.pre("save", async function(){
+    if (!this.isModified("password")) return;
+
+    const hash=await bcrypt.hash(this.password, 10);
+    this.password=hash;
+})
+// This method is used to compare a plain text password with the hashed password stored in the database. 
+// It uses bcrypt's compare function to check if the provided password matches the hashed password. 
+// This is typically used during login to verify the user's credentials.
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
